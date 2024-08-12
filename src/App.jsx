81538dd1c98b8064dc7a9170075ed941370c39a7
bolaji87 <sakabolaji77@gmai.com>
2024-08-12 11:10:37 +0100

    enable state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useReducer } from "react";
+import Header from "./components/Header.jsx";
+import MainArea from "./components/MainArea.jsx";
+import Loader from "./components/Loader.jsx";
+import Error from "./components/Error.jsx";
+import StartScreen from "./components/StartScreen.jsx";
+import Questions from "./components/Questions.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialState = {
+  questions: [],
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  status: "loading",
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      throw new Error("unknown action");
+  }
 }
 
-export default App
+export default function App() {
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+  useEffect(function () {
+    async function getQuestions() {
+      try {
+        const res = await fetch("http://localhost:7000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    getQuestions();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <MainArea>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Questions question={questions[index]} />}
+      </MainArea>
+    </div>
+  );
+}
---
+++ src/components/MainArea.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainArea({ children }) {
+  return <main className="main">{children}</main>;
+}
+
+export default MainArea;

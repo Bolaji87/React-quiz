24fcd2019d6867ce153d652477da17a2b0ff329d
bolaji87 <sakabolaji77@gmai.com>
2024-08-16 16:50:50 +0100

    add question and options components

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Main from "./components/Main.jsx";
 import Loader from "./components/Loader.jsx";
 import Error from "./components/Error.jsx";
 import StartScreen from "./components/StartScreen.jsx";
+import Question from "./components/Question.jsx";
 
 const initialState = {
   questions: [],
   status: "loading",
+  index: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -15,13 +17,18 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("unknown action");
   }
 }
 
 function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -45,7 +52,10 @@ function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen dispatch={dispatch} numQuestions={numQuestions} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3> {numQuestions} to test your React mastery</h3>
-      <button className="btn btn-ui"> Let's start</button>
+      <button
+        onClick={() => dispatch({ type: "start" })}
+        className="btn btn-ui"
+      >
+        {" "}
+        Let's start
+      </button>
     </div>
   );
 }
